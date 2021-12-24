314113d90ec8963356fea86efbcdfa72fb5d5f3c
Jose González <jose@wiris.com>
2021-12-24 17:14:35 +0100

    refactor: Remove unnecessary variables from TS naive original implementation.

--- typescript/day1/part1/naive_solution.ts
+++ typescript/day1/part1/naive_solution.ts
@@ -1,16 +1,13 @@
 import data from '../measurements/data';
 
-let previousIndex = 0;
-let currentIndex = 1;
-
 let increments: number = 0;
+let iterations = 0;
 for (let i = 1; i < data.length; i++) {
-  if (data[previousIndex] < data[currentIndex]) {
+  console.log(`comparing indexes ${i} and ${i - 1}`)
+  if (data[i - 1] < data[i]) {
     increments = increments + 1;
   }
-
-  previousIndex = previousIndex + 1;
-  currentIndex = currentIndex + 1;
+  iterations = iterations + 1;
 }
 
-console.log(`The number of increments are ${increments}`);
+console.log(`The number of increments are ${increments} and iterations: ${iterations}`);
